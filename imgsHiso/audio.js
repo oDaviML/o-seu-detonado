da3216470b3654dd62a1fcdfb4fdb4557361bc99
oDaviML <85958184+oDaviML@users.noreply.github.com>
2021-09-14 10:25:49 -0300

    Add files via upload

---
+++ imgsHiso/audio.js
@@ -0,0 +1,7 @@
+        var x = document.getElementById("iniciar");
+        var audio = document.getElementById("iniciar");
+        audio.volume = 0.1;
+
+        function mouseenter() {
+            x.play();
+        }
